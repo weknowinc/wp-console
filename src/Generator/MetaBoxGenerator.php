33e8bb2903283aa874707c4af4e7e7a449d81edb
hjuarez20 <hjuarez20@gmail.com>
2017-03-21 18:13:28 +0000

    Add files to  metabox command

---
+++ src/Generator/MetaBoxGenerator.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @file
+ * Contains \WP\Console\Generator\MetaBoxGenerator.
+ */
+
+namespace WP\Console\Generator;
+
+use WP\Console\Core\Generator\Generator;
+use WP\Console\Extension\Manager;
+
+/**
+ * Class MetaBoxGenerator
+ *
+ * @package WP\Console\Generator
+ */
+class MetaBoxGenerator extends Generator
+{
+    
+    /**
+     * @var Manager
+     */
+    protected $extensionManager;
+    
+    /**
+     * AuthenticationProviderGenerator constructor.
+     *
+     * @param Manager $extensionManager
+     */
+    public function __construct(
+        Manager $extensionManager
+    ) {
+        $this->extensionManager = $extensionManager;
+    }
+    
+    
+    /**
+     * Generator MetaBox
+     *
+     * @param $plugin,
+     * @param $class_name
+     * @param $metabox_id,
+     * @param $title,
+     * @param $callback_function,
+     * @param $screen,
+     * @param $page_location,
+     * @param $priority,
+     * @param $fields_metabox
+     */
+    public function generate(
+        $plugin,
+        $class_name,
+        $metabox_id,
+        $title,
+        $callback_function,
+        $screen,
+        $page_location,
+        $priority,
+        $fields_metabox
+
+    ) {
+        $parameters = [
+            'plugin' => $plugin,
+            'class_name' => $class_name,
+            'metabox_id' => $metabox_id,
+            'title' => $title,
+            'callback_function' => $callback_function,
+            'screen' => $screen,
+            'page_location' => $page_location,
+            'priority' => $priority,
+            'fields_metabox' => $fields_metabox
+        ];
+        
+        $plugin_lower = strtolower($plugin);
+       
+        var_dump($this->extensionManager->getPlugin($plugin_lower.'/'.$plugin_lower.'.php'));
+   /*     $this->renderFile(
+            'plugin/src/metabox/class-metabox-php.twig',
+            $this->extensionManager->getPlugin($plugin)->getPath().'/'.$plugin.'/admin/'.$class_name.'_meta_box.php',
+            $parameters
+        );
+    
+        $this->renderFile(
+            'plugin/src/metabox/class-metabox-display.php.twig',
+            $this->extensionManager->getPlugin($plugin)->getPath().'/'.$plugin.'/admin/'.$class_name.'_meta_box_display.php',
+            $parameters
+        );*/
+    }
+}
