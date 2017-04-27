ea3fb67d7ec984ea0ff84aad8e610e59fa4ecd16
enzo - Eduardo Garcia <enzo@anexusit.com>
2017-04-28 05:32:14 +1000

    Add Container Aware Command Trait

---
+++ src/Command/Shared/ContainerAwareCommandTrait.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Contains WP\Console\Core\Command\Shared\ContainerAwareCommandTrait.
+ */
+
+namespace WP\Console\Command\Shared;
+
+/**
+ * Class CommandTrait
+ * @package WP\Console\Core\Command
+ */
+trait ContainerAwareCommandTrait
+{
+    use CommandTrait;
+
+    protected $container;
+
+    /**
+     * @param mixed $container
+     */
+    public function setContainer($container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param $key
+     * @return null|object
+     */
+    public function has($key)
+    {
+        if (!$key) {
+            return null;
+        }
+
+        return $this->container->has($key);
+    }
+
+    /**
+     * @param $key
+     * @return null|object
+     */
+    public function get($key)
+    {
+        if (!$key) {
+            return null;
+        }
+
+        if ($this->has($key)) {
+            return $this->container->get($key);
+        }
+
+        return null;
+    }
+}
