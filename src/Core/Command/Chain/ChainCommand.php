da13df3885ab38aff78502ba79b173efd670efdd
enzo - Eduardo Garcia <enzo@anexusit.com>
2017-05-27 04:58:29 +1000

    [core] Include src/functions in composer autoload, include ArrayInput use

--- src/Core/Command/Chain/ChainCommand.php
+++ src/Core/Command/Chain/ChainCommand.php
@@ -11,6 +11,7 @@ use Dflydev\PlaceholderResolver\DataSource\ArrayDataSource;
 use Dflydev\PlaceholderResolver\RegexPlaceholderResolver;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Filesystem\Filesystem;
