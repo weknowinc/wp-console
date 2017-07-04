4e0f94a6ef05bf831e38eceb3e65602e2a24832c
harold20 <hjuarez20@gmail.com>
2017-07-04 13:30:23 -0600

    [generate:post:type] New command (#6)
    * Add new command
    
    * Fix translations and add lost trait
    
    * Fix template and commandTrait
    
    * Fix post type template

---
+++ src/Command/Shared/Taxonomy_PostTypeTrait.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * @file
+ * Contains WP\Console\Command\Shared\Taxonomy_PostTypeTrait.
+ */
+
+namespace WP\Console\Command\Shared;
+
+use WP\Console\Core\Style\WPStyle;
+
+/**
+ * Class PluginTrait
+ *
+ * @package WP\Console\Command
+ */
+trait Taxonomy_PostTypeTrait
+{
+    public function labelsQuestion(WPStyle $io, $labels)
+    {
+        $stringUtils = $this->stringConverter;
+        $label_array = [];
+        foreach ($labels as $label) {
+            if ($io->confirm(
+                $this->trans('commands.generate.posttype.questions.labels-add'). $label,
+                true
+            )
+            ) {
+                $value = $stringUtils->underscoreToCamelCase($label);
+                $value = $stringUtils->camelCaseToHuman($value);
+                $label_array[$label] = $io->ask(
+                    $this->trans('commands.generate.posttype.questions.labels-edit'). $label,
+                    $value
+                );
+
+
+            }
+        }
+
+        return $label_array;
+    }
+
+    public function visibilityQuestion(WPStyle $io, $labels_visibility)
+    {
+        foreach ($labels_visibility as $index => $value) {
+            if ($index != 'menu_position') {
+                $labels_visibility[$index] = $io->confirm(
+                    $this->trans('commands.generate.posttype.questions.visibility-options'). str_replace('show_in_', '', $index),
+                    true
+                );
+            }
+
+            if ($index == 'show_in_menu' && $labels_visibility[$index]) {
+
+                $array_menu_position = ['5' => 'below Post', '10' => 'below Post', '15' => 'below Links', '20' => 'below Pages',
+                    '25' => 'below Comments', '60' => 'below First separator', '65' => 'below Plugins',
+                    '70' => 'below Users', '75' => 'below Tools','80' => 'below Settings', '100' => 'below second separator' ];
+
+                $result = $io->choice(
+                    $this->trans('commands.generate.posttype.questions.labels-edit'),
+                    $array_menu_position
+                );
+
+                $labels_visibility['menu_position'] = array_search($result, $array_menu_position);
+            }
+        }
+
+        return $labels_visibility;
+    }
+
+    public function permalinksQuestion(WPStyle $io, $permalinks)
+    {
+        $stringUtils = $this->stringConverter;
+        $label_array = [];
+        foreach ($permalinks as $permalink) {
+            if ($permalink != 'slug') {
+                $label_array[$permalink] = $io->confirm(
+                    $this->trans('commands.generate.posttype.questions.permalinks-options'). $permalink,
+                    true
+                );
+            } else {
+
+                $value = $io->ask(
+                    $this->trans('commands.generate.posttype.questions.permalinks-slug'),
+                    'post_type'
+                );
+
+                $label_array[$permalink] = $stringUtils->camelCaseToUnderscore($stringUtils->humanToCamelCase($value));
+            }
+        }
+
+        return $label_array;
+    }
+
+    public function capabilitiesQuestion(WPStyle $io, $capabilities_labels)
+    {
+        $stringUtils = $this->stringConverter;
+        $label_array = [];
+        foreach ($capabilities_labels as $label) {
+
+            $value = $io->ask(
+                $this->trans('commands.generate.posttype.questions.capabilities-options').$stringUtils->camelCaseToHuman($stringUtils->underscoreToCamelCase($label)) ,
+                $label
+            );
+
+            $label_array[$label] = $stringUtils->camelCaseToUnderscore($stringUtils->humanToCamelCase($value));
+        }
+        return $label_array;
+    }
+
+    public function restQuestion(WPStyle $io, $type, $key)
+    {
+
+        $show_rest = $io->confirm(
+            $this->trans('commands.generate.posttype.questions.show-rest')
+        );
+
+        $rest_base = $io->ask(
+            $this->trans('commands.generate.posttype.questions.rest-base'),
+            $key
+        );
+
+        $rest_controller_class = $io->ask(
+            $this->trans('commands.generate.posttype.questions.rest-controller-class'),
+            'WP_REST_'.$type.'_Controller'
+        );
+
+        $rests =
+            [
+                'show_in_rest' => $show_rest,
+                'rest_base' => $rest_base,
+                'rest_controller_class' => $rest_controller_class,
+            ];
+
+        return $rests;
+    }
+}
---
+++ src/Generator/PostTypeGenerator.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * @file
+ * Contains \WP\Console\Generator\PostTypeGenerator.
+ */
+
+namespace WP\Console\Generator;
+
+use WP\Console\Core\Generator\Generator;
+use WP\Console\Extension\Manager;
+
+/**
+ * Class PostTypeGenerator
+ *
+ * @package WP\Console\Generator
+ */
+class PostTypeGenerator extends Generator
+{
+
+    /**
+     * @var Manager
+     */
+    protected $extensionManager;
+
+    /**
+     * PortTypeGenerator constructor.
+     *
+     * @param Manager $extensionManager
+     */
+    public function __construct(
+        Manager $extensionManager
+    )
+    {
+        $this->extensionManager = $extensionManager;
+    }
+
+
+    /**
+     * Generator MetaBox
+     *
+     * @param $plugin
+     * @param $class_name
+     * @param $function_name
+     * @param $post_type_key
+     * @param $description
+     * @param $name_singular
+     * @param $name_plural
+     * @param $taxonomy
+     * @param $hierarchical
+     * @param $labels
+     * @param $supports
+     * @param $exclude_from_search
+     * @param $enable_export
+     * @param $enable_archives
+     * @param $visibility
+     * @param $permalinks
+     * @param $capabilities
+     * @param $rest
+     * @param $child_themes
+     */
+    public function generate(
+        $plugin,
+        $class_name,
+        $function_name,
+        $post_type_key,
+        $description,
+        $name_singular,
+        $name_plural,
+        $taxonomy,
+        $hierarchical,
+        $labels,
+        $supports,
+        $exclude_from_search,
+        $enable_export,
+        $enable_archives,
+        $visibility,
+        $permalinks,
+        $capabilities,
+        $rest,
+        $child_themes
+    )
+    {
+        $pluginFile = $this->extensionManager->getPlugin($plugin)->getPathname();
+
+        $parameters = [
+            'plugin' => $plugin,
+            'class_name_post_type' => $class_name,
+            'function_name' => $function_name,
+            'post_type_key' => $post_type_key,
+            'description' => $description,
+            'name_singular' => $name_singular,
+            'name_plural' => $name_plural,
+            'taxonomy' => $taxonomy,
+            'hierarchical' => $hierarchical,
+            'labels' => $labels,
+            'supports' => $supports,
+            'exclude_from_search' => $exclude_from_search,
+            'enable_export' => $enable_export,
+            'enable_archives' => $enable_archives,
+            'visibility' => $visibility,
+            'permalinks' => $permalinks,
+            'capabilities' => $capabilities,
+            'rest' => $rest,
+            'child_theme' => $child_themes,
+            'class_name_post_type_path' => 'admin/' . lcfirst($class_name) . '-post-type.php',
+            'file_exists' => file_exists($pluginFile)
+        ];
+
+        $dir = $this->extensionManager->getPlugin($plugin)->getPath(). '/' . $parameters['class_name_post_type_path'];
+
+        if (file_exists($dir)) {
+            if (!is_dir($dir)) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Unable to generate the metaboxs , it already exist at "%s"',
+                        realpath($dir)
+                    )
+                );
+            }
+        }
+
+        $this->renderFile(
+            'plugin/src/PostType/class-post-type.php.twig',
+            $dir,
+            $parameters
+        );
+
+        $this->renderFile(
+            'plugin/plugin.php.twig',
+            $pluginFile,
+            $parameters,
+            FILE_APPEND
+        );
+    }
+}
