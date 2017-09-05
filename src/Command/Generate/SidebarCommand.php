70f14fa4502aa91a91b1c0b3a16bcbbf7038d324
Harold Juarez <hjuarez20@gmail.com>
2017-09-05 15:43:14 -0600

    [generate:sidebar]New command

---
+++ src/Command/Generate/SidebarCommand.php
@@ -0,0 +1,232 @@
+<?php
+
+/**
+ * @file
+ * Contains \WP\Console\Command\Generate\SidebarCommand.
+ */
+
+namespace WP\Console\Command\Generate;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Command\Command;
+use WP\Console\Command\Shared\ContainerAwareCommandTrait;
+use WP\Console\Command\Shared\ThemeTrait;
+use WP\Console\Command\Shared\ConfirmationTrait;
+use WP\Console\Generator\SidebarGenerator;
+use WP\Console\Core\Utils\StringConverter;
+use WP\Console\Extension\Manager;
+use WP\Console\Core\Style\WPStyle;
+use WP\Console\Utils\Site;
+use WP\Console\Utils\Validator;
+
+class SidebarCommand extends Command
+{
+    use ContainerAwareCommandTrait;
+    use ConfirmationTrait;
+    use ThemeTrait;
+
+    /**
+     * @var SidebarGenerator
+     */
+    protected $generator;
+
+    /**
+     * @var Manager
+     */
+    protected $extensionManager;
+
+    /**
+     * @var Validator
+     */
+    protected $validator;
+
+    /**
+     * @var StringConverter
+     */
+    protected $stringConverter;
+
+    /**
+     * @var Site
+     */
+    protected $site;
+
+    /**
+     * ToolbarCommand constructor.
+     *
+     * @param SidebarGenerator $generator
+     * @param Manager          $extensionManager
+     * @param Validator        $validator
+     * @param StringConverter  $stringConverter
+     * @param Site             $site
+     */
+    public function __construct(
+        SidebarGenerator $generator,
+        Manager $extensionManager,
+        Validator $validator,
+        StringConverter $stringConverter,
+        Site $site
+    ) {
+        $this->generator = $generator;
+        $this->extensionManager = $extensionManager;
+        $this->validator = $validator;
+        $this->stringConverter = $stringConverter;
+        $this->site = $site;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('generate:sidebar')
+            ->setDescription($this->trans('commands.generate.sidebar.description'))
+            ->setHelp($this->trans('commands.generate.sidebar.help'))
+            ->addOption(
+                'theme',
+                '',
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.common.options.theme')
+            )
+
+            ->addOption(
+                'function-name',
+                '',
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.generate.sidebar.options.function-name')
+            )
+            ->addOption(
+                'sidebar-items',
+                '',
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.generate.sidebar.options.sidebar-items')
+            )
+            ->addOption(
+                'child-themes',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                $this->trans('commands.generate.sidebar.options.child-themes')
+            )
+            ->setAliases(['gsb']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new WPStyle($input, $output);
+
+        $theme = $input->getOption('theme');
+        $function_name = $input->getOption('function-name');
+        $sidebar_items = $input->getOption('sidebar-items');
+        $child_themes = $input->getOption('child-themes');
+        $yes = $input->hasOption('yes')?$input->getOption('yes'):false;
+
+        // @see use WP\Console\Command\Shared\ConfirmationTrait::confirmGeneration
+        if (!$this->confirmGeneration($io, $yes)) {
+            return;
+        }
+
+        $this->generator->generate(
+            $theme,
+            $function_name,
+            $sidebar_items,
+            $child_themes,
+            $this->site
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $io = new WPStyle($input, $output);
+
+        // --theme
+        $theme = $input->getOption('theme');
+        if (!$theme) {
+            $theme = $this->themeQuestion($io);
+            $input->setOption('theme', $theme);
+        }
+
+        // --function name
+        $function_name = $input->getOption('function-name');
+        if (!$function_name) {
+            $function_name = $io->ask(
+                $this->trans('commands.generate.sidebar.questions.function-name'),
+                'custom_sidebar'
+            );
+            $input->setOption('function-name', $function_name);
+        }
+
+        // --sidebar items
+        $sidebar_items = $input->getOption('sidebar-items');
+        if (!$sidebar_items) {
+            $array_sidebar_items = [];
+            $stringConverter = $this->stringConverter;
+            while (true) {
+                $id = $io->ask(
+                    $this->trans('commands.generate.sidebar.questions.id'),
+                    str_replace("_", "-", $function_name),
+                    function ($id) use ($stringConverter) {
+                        $id = $stringConverter->humanToCamelCase($id);
+                        return $stringConverter->camelCaseToLowerCase($id);
+                    }
+                );
+
+                $class = $io->ask(
+                    $this->trans('commands.generate.sidebar.questions.class'),
+                    $function_name,
+                    function ($class) use ($stringConverter) {
+                        $class = $stringConverter->humanToCamelCase($class);
+                        return $stringConverter->camelCaseToLowerCase($class);
+                    }
+                );
+
+                $name = $io->ask(
+                    $this->trans('commands.generate.sidebar.questions.name'),
+                    'default sidebar'
+                );
+
+                $description = $io->ask(
+                    $this->trans('commands.generate.sidebar.questions.description'),
+                    'My first Sidebar'
+                );
+
+                array_push(
+                    $array_sidebar_items,
+                    [
+                        'id' => $id,
+                        'class' => $class,
+                        'name' => $name,
+                        'description' => $description,
+                    ]
+                );
+
+                if (!$io->confirm(
+                    $this->trans('commands.generate.sidebar.questions.sidebar-add'),
+                    true
+                )
+                ) {
+                    break;
+                }
+            }
+            $input->setOption('sidebar-items', $array_sidebar_items);
+        }
+
+        // --child themes
+        $child_themes = $input->getOption('child-themes');
+        if (!$child_themes) {
+            $child_themes = $io->confirm(
+                $this->trans('commands.generate.sidebar.questions.child-themes'),
+                false
+            );
+            $input->setOption('child-themes', $child_themes);
+        }
+    }
+}
---
+++ src/Generator/SidebarGenerator.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @file
+ * Contains \WP\Console\Generator\SidebarGenerator.
+ */
+
+namespace WP\Console\Generator;
+
+use WP\Console\Extension\Manager;
+use WP\Console\Core\Utils\TranslatorManager;
+use WP\Console\Core\Generator\Generator;
+
+/**
+ * Class SidebarGenerator
+ *
+ * @package WP\Console\Generator
+ */
+class SidebarGenerator extends Generator
+{
+    /**
+     * @var Manager
+     */
+    protected $extensionManager;
+
+    /**
+     * @var TranslatorManager
+     */
+    protected $translatorManager;
+
+    /**
+     * SidebarGenerator constructor.
+     *
+     * @param Manager $extensionManager
+     */
+    public function __construct(
+        Manager $extensionManager
+    ) {
+        $this->extensionManager = $extensionManager;
+    }
+
+    /**
+     * Generate.
+     *
+     * @param string  $theme
+     * @param string  $function_name
+     * @param string  $sidebar_items
+     * @param boolean $child_themes
+     * @param Site    $site
+     */
+    public function generate(
+        $theme,
+        $function_name,
+        $sidebar_items,
+        $child_themes,
+        $site
+    ) {
+        $discoverThemes = $this->extensionManager->discoverThemes()->showActivated()->showDeactivated()->getList();
+        $extensions = array_combine(array_keys($discoverThemes), array_column($discoverThemes, 'Name'));
+        $themeName = array_search($theme, $extensions);
+
+        $class_name_sidebar = $themeName.'Sidebar/'.$themeName.'Sidebar.php';
+        $themeFile = $this->extensionManager->getTheme($theme)->getPathname();
+        $dir = $this->extensionManager->getTheme($theme)->getPath().'/'.$class_name_sidebar;
+
+        $parameters = [
+            'theme' => $theme,
+            'function_name' => $function_name,
+            'sidebar_items' => $sidebar_items,
+            'child_theme' => $child_themes,
+            'class_name_sidebar_path' => $class_name_sidebar,
+            'file_exists' => file_exists($dir)
+        ];
+
+        $site->loadLegacyFile($dir);
+
+        if (function_exists($function_name)) {
+            throw new \RuntimeException(
+                sprintf(
+                    'Unable to generate the sidebar , The function name already exist at "%s"',
+                    realpath($dir)
+                )
+            );
+        }
+
+        if (!file_exists($themeFile.'/functions.php') && !file_exists($dir)) {
+            $this->renderFile(
+                'theme/functions.php.twig',
+                $themeFile.'/functions.php',
+                $parameters,
+                FILE_APPEND
+            );
+        }
+
+        $this->renderFile(
+            'theme/src/Sidebar/sidebar.php.twig',
+            $dir,
+            $parameters,
+            FILE_APPEND
+        );
+    }
+}
