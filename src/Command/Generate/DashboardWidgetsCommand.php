bb67954fa04425c6f247ed9be21943be33ff3dfa
Harold Juarez <hjuarez20@gmail.com>
2017-12-21 21:14:33 -0600

    [generate:dashboard:widgets] New command

---
+++ src/Command/Generate/DashboardWidgetsCommand.php
@@ -0,0 +1,267 @@
+<?php
+
+/**
+ * @file
+ * Contains \WP\Console\Command\Generate\DashboardWidgetsCommand.
+ */
+
+namespace WP\Console\Command\Generate;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use WP\Console\Command\Shared\ConfirmationTrait;
+use WP\Console\Command\Shared\PluginTrait;
+use WP\Console\Core\Command\Command;
+use WP\Console\Extension\Manager;
+use WP\Console\Generator\DashboardWidgetsGenerator;
+use WP\Console\Core\Style\WPStyle;
+use WP\Console\Utils\Validator;
+use WP\Console\Core\Utils\StringConverter;
+
+class DashboardWidgetsCommand extends Command
+{
+    use PluginTrait;
+    use ConfirmationTrait;
+
+    /**
+     * @var DashboardWidgetsGenerator
+     */
+    protected $generator;
+
+    /**
+     * @var Validator
+     */
+    protected $validator;
+
+    /**
+     * @var Manager
+     */
+    protected $extensionManager;
+
+    /**
+     * @var StringConverter
+     */
+    protected $stringConverter;
+
+    /**
+     * DashboardWidgetsCommand constructor.
+     *
+     * @param DashboardWidgetsGenerator $generator
+     * @param Manager                   $extensionManager
+     * @param Validator                 $validator
+     * @param StringConverter           $stringConverter
+     */
+    public function __construct(
+        DashboardWidgetsGenerator $generator,
+        Manager $extensionManager,
+        Validator $validator,
+        StringConverter $stringConverter
+    ) {
+        $this->generator = $generator;
+        $this->extensionManager = $extensionManager;
+        $this->validator = $validator;
+        $this->stringConverter = $stringConverter;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('generate:dashboard:widgets')
+            ->setDescription($this->trans('commands.generate.dashboard.widgets.description'))
+            ->setHelp($this->trans('commands.generate.dashboard.widgets.help'))
+            ->addOption(
+                'plugin',
+                '',
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.common.options.plugin')
+            )
+            ->addOption(
+                'class-name',
+                '',
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.generate.dashboard.widgets.options.class-name')
+            )
+            ->addOption(
+                'id',
+                '',
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.generate.dashboard.widgets.options.id')
+            )
+            ->addOption(
+                'title',
+                '',
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.generate.dashboard.widgets.options.title')
+            )
+            ->addOption(
+                'render-function',
+                '',
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.generate.dashboard.widgets.options.render-function')
+            )
+            ->addOption(
+                'submission-function',
+                '',
+                InputOption::VALUE_OPTIONAL,
+                $this->trans('commands.generate.dashboard.widgets.options.submission-function')
+            )
+            ->addOption(
+                'callback-arguments',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                $this->trans('commands.generate.dashboard.widgets.options.callback-arguments')
+            )
+            ->addOption(
+                'text-domain',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                $this->trans('commands.generate.dashboard.widgets.options.text-domain')
+            )
+            ->setAliases(['gdw']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new WPStyle($input, $output);
+
+        // @see use WP\Console\Command\Shared\ConfirmationTrait::confirmGeneration
+        if (!$this->confirmGeneration($io)) {
+            return;
+        }
+
+        $plugin = $plugin = $this->validator->validatePluginName($input->getOption('plugin'));
+        $class_name = $this->validator->validateClassName($input->getOption('class-name'));
+        $id = $this->validator->validateMachineName($input->getOption('id'));
+        $title = $input->getOption('title');
+        $render_function = $this->validator->validateFunctionName($input->getOption('render-function'));
+        $submission_function = $input->getOption('submission-function');
+        $callback_arguments = $input->getOption('callback-arguments');
+        $text_domain = $input->getOption('text-domain');
+
+
+
+        $this->generator->generate(
+            $plugin,
+            $class_name,
+            $id,
+            $title,
+            $render_function,
+            $submission_function,
+            $callback_arguments,
+            $text_domain
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $io = new WPStyle($input, $output);
+
+        // --plugin
+        $plugin = $input->getOption('plugin');
+        if (!$plugin) {
+            $plugin = $this->pluginQuestion($io);
+            $input->setOption('plugin', $plugin);
+        }
+
+        // --class name
+        $class_name = $input->getOption('class-name');
+        if (!$class_name) {
+            $class_name = $io->ask(
+                $this->trans('commands.generate.dashboard.widgets.questions.class-name'),
+                'CustomDashboardWidgets',
+                function ($value) {
+                    if (!strlen(trim($value))) {
+                        throw new \Exception('The Class name can not be empty');
+                    }
+                    return $this->stringConverter->humanToCamelCase($value);
+                }
+            );
+            $input->setOption('class-name', $class_name);
+        }
+
+        // --id
+        $id = $input->getOption('id');
+        if (!$id) {
+            $id = $io->ask(
+                $this->trans('commands.generate.dashboard.widgets.questions.id'),
+                strtolower($class_name),
+                function ($value) {
+                    return $this->stringConverter->createMachineName($value);
+                }
+            );
+        }
+        $input->setOption('id', $id);
+
+        // --title
+        $title = $input->getOption('title');
+        if (!$title) {
+            $title = $io->ask(
+                $this->trans('commands.generate.dashboard.widgets.questions.title'),
+                'Example Dashboard Widget'
+            );
+        }
+        $input->setOption('title', $title);
+
+        // --render function
+        $render_function = $input->getOption('render-function');
+        if (!$render_function) {
+            $render_function = $io->ask(
+                $this->trans('commands.generate.dashboard.widgets.questions.render-function'),
+                $id.'_callback',
+                function ($value) {
+                    return $this->validator->validateFunctionName($value);
+                }
+            );
+        }
+        $input->setOption('render-function', $render_function);
+
+        // --submission function
+        $submission_function = $input->getOption('submission-function');
+        if (!$submission_function) {
+            $submission_function = $io->confirm(
+                $this->trans('commands.generate.dashboard.widgets.questions.submission-function'),
+                false
+            );
+        }
+        $input->setOption('submission-function', $submission_function);
+
+        // --callback arguments
+        $callback_arguments = $input->getOption('callback-arguments');
+        if (!$callback_arguments) {
+            $callback_arguments = $io->askEmpty(
+                $this->trans('commands.generate.dashboard.widgets.questions.callback-arguments'),
+                function ($value) {
+                    if (!empty($value)) {
+                        $validate = array($value);
+                        if (!is_array($validate)) {
+                            throw new \Exception('Do not have the correct format');
+                        }
+                    }
+                    return $value;
+                }
+            );
+        }
+        $input->setOption('callback-arguments', $callback_arguments);
+
+        // --text domain
+        $text_domain = $input->getOption('text-domain');
+        if (!$text_domain) {
+            $text_domain = $io->askEmpty(
+                $this->trans('commands.generate.dashboard.widgets.questions.text-domain')
+            );
+        }
+        $input->setOption('text-domain', $text_domain);
+    }
+}
---
+++ src/Generator/DashboardWidgetsGenerator.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * @file
+ * Contains \WP\Console\Generator\DashboardWidgetsGenerator.
+ */
+
+namespace WP\Console\Generator;
+
+use WP\Console\Core\Generator\Generator;
+use WP\Console\Extension\Manager;
+
+/**
+ * Class DashboardWidgetsGenerator
+ *
+ * @package WP\Console\Generator
+ */
+class DashboardWidgetsGenerator extends Generator
+{
+    
+    /**
+     * @var Manager
+     */
+    protected $extensionManager;
+    
+    /**
+     * DashboardWidgetsGenerator constructor.
+     *
+     * @param Manager $extensionManager
+     */
+    public function __construct(
+        Manager $extensionManager
+    ) {
+        $this->extensionManager = $extensionManager;
+    }
+    
+    
+    /**
+     * Generator Dashboard Widgets
+     *
+     * @param string $plugin,
+     * @param string $class_name
+     * @param string $id
+     * @param string $title
+     * @param string $render_function
+     * @param string $submission_function
+     * @param array  $callback_arguments
+     * @param string $text_domain
+     */
+    public function generate(
+        $plugin,
+        $class_name,
+        $id,
+        $title,
+        $render_function,
+        $submission_function,
+        $callback_arguments,
+        $text_domain
+    ) {
+        $pluginFile = $this->extensionManager->getPlugin($plugin)->getPathname();
+        $dir = $this->extensionManager->getPlugin($plugin)->getPath();
+
+        $parameters = [
+            "plugin" => $plugin,
+            "class_name" => $class_name,
+            "id" => $id,
+            "title" => $title,
+            "render_function" => $render_function,
+            "submission_function" => $submission_function,
+            "callback_arguments" => $callback_arguments,
+            "text_domain" => $text_domain,
+            "class_name_path" => 'DashboardWidgets/' . lcfirst($class_name) . '.php',
+            "admin_dashboard_widgets_path" => 'admin/partials/dashboard-widgets-admin.php',
+            "file_exists" => file_exists($pluginFile),
+            "command_name" => 'dashboard_widgets'
+        ];
+
+        $file_path = $dir.'/admin/partials/'.$parameters['class_name_path'];
+        $file_path_admin = $dir.'/'.$parameters['admin_dashboard_widgets_path'];
+        $parameters['admin_file_exists'] = file_exists($file_path_admin);
+
+        if (file_exists($file_path)) {
+            if (!is_dir($file_path)) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Unable to generate dashboard widgets , it already exist at "%s"',
+                        realpath($file_path)
+                    )
+                );
+            }
+        }
+
+        if (!file_exists($file_path_admin)) {
+            $this->renderFile(
+                'plugin/plugin.php.twig',
+                $pluginFile,
+                $parameters,
+                FILE_APPEND
+            );
+        }
+        
+        $this->renderFile(
+            'plugin/src/Widget/class-dashboard-widgets.php.twig',
+            $file_path,
+            $parameters
+        );
+
+        $this->renderFile(
+            'plugin/src/class-admin.php.twig',
+            $file_path_admin,
+            $parameters,
+            FILE_APPEND
+        );
+    }
+}
